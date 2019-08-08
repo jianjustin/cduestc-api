15a45344a646c4ebcfb52f5ae311a2af4b4bab05
jian <janine.jian.chen@gmail.com>
2019-08-08 16:29:48 +0800

    change response charset

--- api/student.js
+++ api/student.js
@@ -1,15 +1,15 @@
 'use strict'
 
 var axiosUtils = require('../utils/axios-utils.js');
+const iconvLite = require('iconv-lite');
 
 exports.login = function(req, res) {
-    console.log("req.params.zjh : " + req.param('zjh'));
-    console.log("req.params.mm : " + req.param('mm'));
     var params = {};
     params.zjh = req.param('zjh');
     params.mm = req.param('mm');
     axiosUtils.get("/loginAction.do", params, 1000, function(response){
-    	console.log(response.data);
+    	let html = iconvLite.decode(response.data, "gb2312");
+    	console.log(html);
     }, function(error){
     	console.log(error);
     });
--- utils/axios-utils.js
+++ utils/axios-utils.js
@@ -2,25 +2,26 @@
 
 var _axios = require('axios');
 var config = require('../config.json');
-_axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
-
+_axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf8';
 
 
 exports.get = function(url, params, timeout, successfn, errorfn) {
+	debugger
     _axios.get(url, {
-    		baseURL: config.cduestc.url,
-    		method: 'GET'
+            baseURL: config.cduestc.url,
+            method: 'GET',
             params: params,
+            responseType: "arraybuffer",
             timeout: timeout
         })
         .then(successfn)
         .catch(errorfn);
 }
 
-exports.post = function(url, params,data,timeout, successfn, errorfn) {
+exports.post = function(url, params, data, timeout, successfn, errorfn) {
     _axios.get(url, {
-    		baseURL: config.cduestc.url,
-    		method: 'POST'
+            baseURL: config.cduestc.url,
+            method: 'POST',
             params: params,
             data: data,
             timeout: timeout
